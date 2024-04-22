5a8191bb1c1fe3a19f10752446a3cae84a49ca7e
unknown <mckeld3@student.op.ac.nz>
2024-04-22 16:39:43 +1200

    ADD: progress to backend

--- app.js
+++ app.js
@@ -6,15 +6,38 @@ import cors from 'cors';
 
 // Import the index routes module
 import indexRoutes from './routes/index.js';
+import institutionRoutes from "./routes/institution.js";
 
 // Create an Express application
 const app = express();
 
+const setXContentTypeOptions = (req, res, next) => {
+  res.set("x-content-type-options", "nosniff");
+  next();
+};
+
+const setXFrameOptions = (req, res, next) => {
+  res.set("x-frame-options", "deny");
+  next();
+};
+
+const setContentSecurityPolicy = (req, res, next) => {
+  res.set("content-security-policy", "default-src 'none'");
+  next();
+};
+
 // Use the CORS module
 app.use(cors());
+app.use(setXContentTypeOptions);
+app.use(setXFrameOptions);
+app.use(setContentSecurityPolicy);
+app.use(express.urlencoded({ extended: false })); // To parse the incoming requests with urlencoded payloads. For example, form data
+app.use(express.json()); // To parse the incoming requests with JSON payloads. For example, REST API requests
+
 
 // Use the routes module
 app.use('/', indexRoutes);
+app.use("/api/institutions", institutionRoutes);
 
 // Start the server on port 3000
 app.listen(3000, () => {
